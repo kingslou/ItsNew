78ba572e388e437c89326e3281680e0fb2ae26a2
kingslou <loujin_007@126.com>
2015-03-23 16:30:29 +0800

    commit

--- src/com/loujin/itsnew/StartActivity.java
+++ src/com/loujin/itsnew/StartActivity.java
@@ -1,18 +1,23 @@
 package com.loujin.itsnew;
-import cn.trinea.android.common.util.ResourceUtils;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.util.Log;
-
 public class StartActivity extends Activity {
-
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
-		
+//		new Thread(new Runnable() {
+//			
+//			@Override
+//			public void run() {
+//				// TODO Auto-generated method stub
+//				 HttpResponse response = http.httpGet(Const.IpAddress);
+//		    	 Log.e("hehe", response.getResponseCode()+"头部"+response.getExpiresHeader()+"缓存"+response.isInCache()+"是否过期"+response.isExpired());
+//			}
+//		}).start();
 		new Handler().postDelayed(new Runnable() {
 		      @Override
 		      public void run() {
